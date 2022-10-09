ac16a37734c092dd085b1c589292b906b16a4252
Radim Janoš <radim.janos@apitree.cz>
2022-10-09 16:07:32 +0200

    switch_world

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,18 @@ fn main() {
             ..default()
         })
         .insert_resource(ImageSettings::default_nearest()) // prevents blurry sprites
+        .insert_resource(CurrentWorld(InWorld::W1Main))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(move_player)
         .add_system(update_transform_from_velocity)
         .add_system(animate_sprite_system_velocity)
+        .add_system(switch_world)
         .run();
 }
 
+// for binding the player with the camera
+// a camera as a child doesn't show a relative scale, nor probably other movement like jumping
 #[derive(Component)]
 struct MovePlayer;
 
@@ -24,6 +28,14 @@ struct AnimationTimer(Timer);
 #[derive(Component, Default)]
 struct Velocity(Vec3);
 
+// For switching between visible worlds
+#[derive(Component, PartialEq)]
+enum InWorld {
+    W1Main,
+    W2,
+}
+struct CurrentWorld(InWorld);
+
 fn setup(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
@@ -32,6 +44,8 @@ fn setup(
     let texture_handle = asset_server.load("textures/rpg/chars/gabe/gabe-idle-run.png");
     let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(24.0, 24.0), 7, 1);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    // Camera
     commands
         .spawn_bundle(Camera2dBundle::default())
         .insert(MovePlayer)
@@ -44,24 +58,41 @@ fn setup(
             transform: Transform::from_scale(Vec3::splat(6.0)),
             ..default()
         })
-        .insert(AnimationTimer(Timer::from_seconds(0.1, true)))
+        .insert(AnimationTimer(Timer::from_seconds(0.08, true)))
         .insert(MovePlayer)
         .insert(Velocity::default());
 
-    commands.spawn_bundle(SpriteBundle {
-        sprite: Sprite {
-            color: Color::rgb(0.7, 0.7, 0.7),
-            custom_size: Some(Vec2::new(200.0, 50.0)),
+    // Sprite
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.7, 0.7, 0.7),
+                custom_size: Some(Vec2::new(200.0, 50.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(0., -100., 0.)),
             ..default()
-        },
-        transform: Transform::from_translation(Vec3::new(0., -100., 0.)),
-        ..default()
-    });
+        })
+        .insert(InWorld::W1Main);
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(0.7, 0.7, 0.1),
+                custom_size: Some(Vec2::new(50.0, 200.0)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(200., 0., 0.)),
+            visibility: Visibility { is_visible: false },
+            ..default()
+        })
+        .insert(InWorld::W2);
 }
 
 fn move_player(
     keyboard_input: Res<Input<KeyCode>>,
     mut query: Query<&mut Velocity, With<MovePlayer>>,
+    mut current_world: ResMut<CurrentWorld>,
 ) {
     for mut velocity in &mut query {
         const SPEED: f32 = 5.;
@@ -73,10 +104,18 @@ fn move_player(
 
         if keyboard_input.pressed(KeyCode::Left) {
             velocity.0 += Vec3::new(-SPEED, 0., 0.);
+
+            // TODO:
+            // after collision detection, create doors to change the current world
+            // enter W1
+            current_world.0 = InWorld::W1Main;
         }
 
         if keyboard_input.pressed(KeyCode::Right) {
             velocity.0 += Vec3::new(SPEED, 0., 0.);
+
+            // enter W2
+            current_world.0 = InWorld::W2;
         }
 
         if keyboard_input.pressed(KeyCode::Up) {
@@ -116,7 +155,19 @@ fn animate_sprite_system_velocity(
             sprite.index = 0;
         } else if timer.finished() {
             let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-            sprite.index = (sprite.index as usize + 1) % texture_atlas.textures.len();
+            sprite.index = (sprite.index + 1) % texture_atlas.textures.len();
+        }
+    }
+}
+
+fn switch_world(current_world: Res<CurrentWorld>, mut query: Query<(&mut Visibility, &InWorld)>) {
+    if current_world.is_changed() {
+        for (mut visibility, in_world) in &mut query {
+            visibility.is_visible = if in_world == &current_world.0 {
+                true
+            } else {
+                false
+            }
         }
     }
 }
