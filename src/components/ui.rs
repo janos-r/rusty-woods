61aa5cc05ea495fdc5980ff145d9464a2ad01385
Radim <radim.janos@seznam.cz>
2023-01-11 19:40:05 +0100

    cspell spellcheck

--- src/components/ui.rs
+++ src/components/ui.rs
@@ -84,7 +84,6 @@ pub fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
             .insert(TextBox)
             .insert(font_handle.clone())
             .with_children(spawn_children_text(font_handle, String::from(
-                // "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularized in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
                 "Text Example a little longer trying to cross the width. Text Example a little longer trying to cross the width"
             )));
         });
