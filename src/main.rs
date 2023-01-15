bee06853f106c1bed0ef2d8967efc9ccc0ac6313
Radim <radim.janos@seznam.cz>
2023-01-15 17:00:27 +0100

    delete MySpriteBundle test sprites

--- src/main.rs
+++ src/main.rs
@@ -73,12 +73,6 @@ impl Plugin for SpawnPlugin {
     }
 }
 
-#[derive(Bundle)]
-struct MySpriteBundle {
-    sprite_bundle: SpriteBundle,
-    collider: Collider,
-}
-
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Camera
     commands.spawn({
@@ -96,41 +90,4 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         ldtk_handle: asset_server.load("LDtk/world2.ldtk"),
         ..default()
     });
-
-    /*
-    // Test sprites
-    commands
-        .spawn(MySpriteBundle {
-            sprite_bundle: SpriteBundle {
-                sprite: Sprite {
-                    color: Color::rgb(0.7, 0.7, 0.7),
-                    custom_size: Some(Vec2::new(200.0, 50.0)),
-                    ..default()
-                },
-                transform: Transform::from_translation(Vec3::new(0., -100., 0.)),
-                ..default()
-            },
-            collider: Collider::cuboid(100.0, 25.0),
-        })
-        .insert(ActiveEvents::COLLISION_EVENTS)
-        .insert(DoorRef {
-            target_level_iid: LevelSelection::Iid(
-                "cb1238d0-5110-11ed-b5f8-774d9bde9a1c".to_owned(),
-            ),
-            target_entity_iid: EntityIid("4618b090-7820-11ed-8a70-c9c419731504".to_owned()),
-        });
-
-    commands.spawn(MySpriteBundle {
-        sprite_bundle: SpriteBundle {
-            sprite: Sprite {
-                color: Color::rgb(0.7, 0.7, 0.1),
-                custom_size: Some(Vec2::new(50.0, 200.0)),
-                ..default()
-            },
-            transform: Transform::from_translation(Vec3::new(200., 0., 0.)),
-            ..default()
-        },
-        collider: Collider::cuboid(25.0, 100.0),
-    });
-    */
 }
